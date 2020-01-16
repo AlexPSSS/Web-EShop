64b6cec2f8c960672db72650bc378a279f0209ee
Попов Александр <bbc@tut.by>
2020-01-16 23:50:03 +0300

    Уже с товарами

---
+++ WebStore/Controllers/GoodsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class GoodsController : Controller
+    {
+        private readonly List<Goods> _goods = new List<Goods>
+        {
+            new Goods
+            {
+                Id = 1,
+                Description = "Молоток",
+                EAN13 = "4602564782511",
+                Group = 100,
+                Price = 22.34F
+            },
+            new Goods
+            {
+                Id = 2,
+                Description = "Дрель",
+                EAN13 = "4602564782619",
+                Group = 202,
+                Price = 100500.01F
+            }
+        };
+
+        // GET: /
+        // GET: /goods/
+        // GET: /goods/index
+        public IActionResult Index()
+        {
+            return View(_goods);
+        }
+
+        // GET: /goods/details/{id}
+        public IActionResult Details(int id)
+        {
+            var goods = _goods.FirstOrDefault(x => x.Id == id);
+
+            //Если такого не существует
+            if (goods == null)
+                return NotFound(); // возвращаем результат 404 Not Found
+
+            return View(goods);
+        }
+
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +8,18 @@ namespace WebStore
 {
     public class Startup
     {
+        //private readonly IConfiguration _configuration;
+
+        //public Startup(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +34,22 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Goods}/{action=Index}/{id?}");
+                // ћаршрут по умолчанию состоит из трЄх частей разделЄнных У/Ф
+                // ѕервой частью указываетс€ им€ контроллера,
+                // второй - им€ действи€ (метода) в контроллере,
+                // третей - опциональный параметр с именем УidФ
+                // ≈сли часть не указана - используютс€ значени€ по умолчанию:
+                // дл€ контроллера им€ УHomeФ,
+                // дл€ действи€ - УIndexФ
+
             });
         }
     }
