51396ed638158259b37a4139ad4ce811c5be40ac
Попов Александр <bbc@tut.by>
2020-01-30 22:52:26 +0300

    Component Cats added

--- WebStore/Infrastructure/Implementations/InMemoryProductService.cs
+++ WebStore/Infrastructure/Implementations/InMemoryProductService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities;
+using WebStore.Domain.Filters;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
 
