4e0c69d01ac9a4bd8f6f2e996e1e5d5da1dbe084
Попов Александр <bbc@tut.by>
2020-02-01 19:21:23 +0300

    DAL added

--- WebStore.DomainNew/Entities/Category.cs
+++ WebStore.DomainNew/Entities/Category.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.DomainNew.Entities.Base;
 using WebStore.DomainNew.Entities.Base.Interfaces;
 
 namespace WebStore.DomainNew.Entities
 {
+    [Table("Categories")]
     public class Category : NamedEntity, IOrderedEntity
     {
         /// <summary>
@@ -14,5 +16,9 @@ namespace WebStore.DomainNew.Entities
         public int? ParentId { get; set; }
 
         public int Order { get; set; }
+
+        [ForeignKey("ParentId")]
+        public virtual Category ParentCategory { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
