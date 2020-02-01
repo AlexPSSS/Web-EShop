fe9196012c14934a7f55c8b828319f4ab2d4df79
Попов Александр <bbc@tut.by>
2020-02-01 19:21:23 +0300

    DAL added

--- WebStore.DomainNew/Entities/Product.cs
+++ WebStore.DomainNew/Entities/Product.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.DomainNew.Entities.Base;
 using WebStore.DomainNew.Entities.Base.Interfaces;
 
 namespace WebStore.DomainNew.Entities
 {
+    [Table("Products")]
     public class Product : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
@@ -13,5 +15,11 @@ namespace WebStore.DomainNew.Entities
         public int? BrandId { get; set; }
         public string ImageUrl { get; set; }
         public decimal Price { get; set; }
+
+        [ForeignKey("CategoryId")] // не обязательно в целом
+        public virtual Category Category { get; set; }
+
+        [ForeignKey("BrandId")]
+        public virtual Brand Brand { get; set; }
     }
 }
