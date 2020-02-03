8adecc041103d1f42a7be61a4a022a1f485c98a0
Попов Александр <bbc@tut.by>
2020-02-03 12:26:45 +0300

    Login/out VC added

---
+++ WebStore/ViewComponents/LogInOutViewComponents.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ViewComponents
+{
+    [ViewComponent(Name = "LoginLogout")]
+    public class LogInOutViewComponents : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
