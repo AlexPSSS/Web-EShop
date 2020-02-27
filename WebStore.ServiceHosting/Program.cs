90bc189c78025b2baa4e638c0562ac7a1f88fa2f
Попов Александр <bbc@tut.by>
2020-02-27 03:10:43 +0300

    SQL migrated to WebAPI

--- WebStore.ServiceHosting/Program.cs
+++ WebStore.ServiceHosting/Program.cs
@@ -1,18 +1,54 @@
 using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebStore.DAL;
 
 namespace WebStore.ServiceHosting
 {
+    //public class Program
+    //{
+    //    public static void Main(string[] args) =>
+    //        CreateWebHostBuilder(args)
+    //            .Build()
+    //            .Run();
+
+    //    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    //        WebHost.CreateDefaultBuilder(args)
+    //            .UseStartup<Startup>();
+    //}
+
     public class Program
     {
-        public static void Main(string[] args) =>
-            CreateWebHostBuilder(args)
-                .Build()
-                .Run();
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            using (var scope = host.Services.CreateScope()) // нужно для получения DbContext
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    WebStoreContext context = services.GetRequiredService<WebStoreContext>();
+                    DbInitializer.Initialize(context);
+                    DbInitializer.InitializeUsers(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Oops. Something went wrong at DB initializing...");
+                }
+            }
+
+            host.Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .Build();
+
     }
+
 }
--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -7,35 +7,48 @@ using WebStore.DAL;
 
 namespace WebStore
 {
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        var host = BuildWebHost(args);
+
+    //        using (var scope = host.Services.CreateScope()) // нужно для получения DbContext
+    //        {
+    //            var services = scope.ServiceProvider;
+    //            try
+    //            {
+    //                WebStoreContext context = services.GetRequiredService<WebStoreContext>();
+    //                DbInitializer.Initialize(context);
+    //                DbInitializer.InitializeUsers(services);
+    //            }
+    //            catch (Exception ex)
+    //            {
+    //                var logger = services.GetRequiredService<ILogger<Program>>();
+    //                logger.LogError(ex, "Oops. Something went wrong at DB initializing...");
+    //            }
+    //        }
+
+    //        host.Run();
+    //    }
+
+    //    private static IWebHost BuildWebHost(string[] args) =>
+    //        WebHost.CreateDefaultBuilder(args)
+    //            .UseStartup<Startup>()
+    //            .Build();
+
+    //}
+
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = BuildWebHost(args);
+        public static void Main(string[] args) =>
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
 
-            using (var scope = host.Services.CreateScope()) // нужно для получения DbContext
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    WebStoreContext context = services.GetRequiredService<WebStoreContext>();
-                    DbInitializer.Initialize(context);
-                    DbInitializer.InitializeUsers(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "Oops. Something went wrong at DB initializing...");
-                }
-            }
-
-            host.Run();
-        }
-
-        private static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-
+                .UseStartup<Startup>();
     }
+
 }
