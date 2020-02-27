2c31bf74fd8019914b878840d47f44e5425dc9df
Попов Александр <bbc@tut.by>
2020-02-27 11:38:19 +0300

    Comments prepared, Swagger added

--- WebStore.Domain/DTO/Products/ProductDTO.cs
+++ WebStore.Domain/DTO/Products/ProductDTO.cs
@@ -5,20 +5,49 @@ using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.DTO.Products
 {
+    //public class ProductDTO : INamedEntity, IOrderedEntity
+    //{
+    //    public int Id { get; set; }
+
+    //    public string Name { get; set; }
+
+    //    public int Order { get; set; }
+
+    //    public decimal Price { get; set; }
+
+    //    public string ImageUrl { get; set; }
+
+    //    public BrandDTO Brand { get; set; }
+
+    //    /// <summary>Сведения о секции товара в каталоге</summary>
+    //    public SectionDTO Section { get; set; }
+    //}
+
+
+    /// <summary>Информация о товаре</summary>
     public class ProductDTO : INamedEntity, IOrderedEntity
     {
+        /// <summary>Идентификатор товара в каталоге</summary>
         public int Id { get; set; }
 
+        /// <summary>Название товара</summary>
         public string Name { get; set; }
 
+        /// <summary>Порядковый номер для сортировки</summary>
         public int Order { get; set; }
 
+        /// <summary>Цена</summary>
         public decimal Price { get; set; }
 
+        /// <summary>Адрес изображения</summary>
         public string ImageUrl { get; set; }
 
+        /// <summary>Сведения о бренде</summary>
         public BrandDTO Brand { get; set; }
 
+        /// <summary>Сведения о секции товара в каталоге</summary>
         public SectionDTO Section { get; set; }
     }
+
+
 }
--- WebStore.ServiceHosting/Controllers/ProductsApiController.cs
+++ WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -12,31 +12,34 @@ using WebStore.Domain.Filters;
 
 namespace WebStore.ServiceHosting.Controllers
 {
+    /// <summary>Контроллер каталога товаров</summary>
     [Route(WebAPI.Products)]
     [ApiController]
     public class ProductsApiController : ControllerBase, IProductService
     {
         private readonly IProductService _ProductData;
 
-        public ProductsApiController(IProductService ProductData) => 
-            _ProductData = ProductData;
+        public ProductsApiController(IProductService ProductData) => _ProductData = ProductData;
 
-        //[HttpGet("categories")]
-        //public IEnumerable<Category> GetSections() => 
-        //    _ProductData.GetCategories();
-
-
+        /// <summary>Получение всех разделов каталога товаров</summary>
+        /// <returns>Перечисление всех разделов каталога</returns>
         [HttpGet("categories")]
-        public IEnumerable<Category> GetCategories() => 
-            _ProductData.GetCategories();
+        public IEnumerable<Category> GetCategories() => _ProductData.GetCategories();
 
-
+        /// <summary>Получение всех брендов товаров из каталога</summary>
+        /// <returns>Перечисление брендов товаров каталога</returns>
         [HttpGet("brands")]
         public IEnumerable<Brand> GetBrands() => _ProductData.GetBrands();
 
+        /// <summary>Получение товаров, удовлетворяющих критерию поиска</summary>
+        /// <param name="Filter">Фильтр - критерий поиска товаров в каталоге</param>
+        /// <returns>Перечисление всех товаров из каталога, удовлетворяющих критерию поиска</returns>
         [HttpPost, ActionName("Post")]
         public IEnumerable<ProductDTO> GetProducts([FromBody] ProductFilter Filter = null) => _ProductData.GetProducts(Filter);
 
+        /// <summary>Получение информации по товару, заданному идентификатором</summary>
+        /// <param name="id">Идентификатор товара, информацию по которому требуется получить</param>
+        /// <returns>Информацию по товару, заданному идентификатором</returns>
         [HttpGet("{id}"), ActionName("Get")]
         public ProductDTO GetProductById(int id) => _ProductData.GetProductById(id);
     }
