9f3dc7029d110d309d0c529da63bebf6494e6103
Попов Александр <bbc@tut.by>
2020-03-08 00:21:33 +0300

    TagHelper is-active-route added

---
+++ WebStore.Domain/WebStoreUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain
+{
+    public static class WebStoreUI
+    {
+
+        public const string AttributeName = "is-active-route";
+
+
+    }
+
+}
