2ad0b46dcfbd29ba60e43014c9b38912f2c62f66
Попов Александр <bbc@tut.by>
2020-03-09 00:55:43 +0300

    Category selection made

---
+++ WebStore.Domain/Models/CategoryCompleteViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Models
+{
+    public class CategoryCompleteViewModel
+    {
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+        public int? CurrentParentCategoryId { get; set; }
+
+        public int? CurrentCategoryId { get; set; }
+    }
+
+}
